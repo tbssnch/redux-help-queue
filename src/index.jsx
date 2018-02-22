78e45f00a14eb4186a1ab6e17f89096eafa9ce5c
Tyler Bussanich <tbussanich@gmail.com>
2018-02-21 18:02:43 -0800

    finish lesson

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import ticketListReducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(ticketListReducer);
+
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
       <HashRouter>
-        <Component/>
-      </HashRouter>
-    </AppContainer>,
+        <Provider store={store}>
+          <Component/>
+        </Provider>
+      </HashRouter>,
     document.getElementById('react-app-root')
   );
 };
